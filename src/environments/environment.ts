37acdfaed50db1fb217649e47cb590022e35e8c7
Ed O'Connor <ed@oconnor.app>
2023-11-29 15:10:00 -0500

    route change 2

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD5uzdhOhZWZA8cbGnieRy62JXWRuK1hGo',
+    authDomain: 'quotes-xl.firebaseapp.com',
+    projectId: 'quotes-xl',
+    storageBucket: 'quotes-xl.appspot.com',
+    messagingSenderId: '98670317502',
+    appId: '1:98670317502:web:e3e3b3302e3f386e5673ef',
+    measurementId: 'G-758F4YE5DH',
+  },
+  api_key: 'c0fcc855119ddc17b3c9c149e9bca66c',
+};
